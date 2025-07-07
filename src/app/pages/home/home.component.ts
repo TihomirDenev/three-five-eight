de4de87f079c9b258cfe39c465d493a1721f3ca0
Tihomir Denev <tihomir.denev@cleverpine.com>
2025-07-07 13:39:34 +0300

    add home content

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-
-import { TranslateModule } from '@ngx-translate/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  imports: [TranslateModule],
+  // imports: [TranslateModule], // Add if you need translations
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(readonly router: Router) {}
+
+  goToPlayers() {
+    this.router.navigate(['/players']);
+  }
+
+  goToRules() {
+    this.router.navigate(['/rules']);
+  }
+}
