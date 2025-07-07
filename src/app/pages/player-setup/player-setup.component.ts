499dbc5638abdc36ab0705c2a6771ae6bf251ef3
Tihomir Denev <tihomir.denev@cleverpine.com>
2025-07-07 13:09:38 +0300

    add player setup content

--- src/app/pages/player-setup/player-setup.component.ts
+++ src/app/pages/player-setup/player-setup.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { Player } from 'src/app/models/player.model';
 
 @Component({
   selector: 'app-player-setup',
-  imports: [],
   templateUrl: './player-setup.component.html',
-  styleUrl: './player-setup.component.scss'
+  styleUrl: './player-setup.component.scss',
+  imports: [FormsModule]
 })
-export class PlayerSetupComponent {
+export class PlayerSetupComponent implements OnInit {
+  players: Player[] = [];
 
+  constructor(readonly router: Router) {}
+
+  ngOnInit() {
+    this.players = [
+    { name: '', quota: 3 },
+    { name: '', quota: 5 },
+    { name: '', quota: 8 }
+  ];
+  }
+
+  startGame() {
+    this.router.navigate(['/board']);
+  }
 }
