666ca20e1978cabbe0747f89eadb0d74795dcd8e
Tihomir Denev <tihomir.denev@cleverpine.com>
2025-07-07 13:39:07 +0300

    add title in header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,25 @@
 import { Component } from "@angular/core";
+import { Router, RouterLink } from "@angular/router";
 
 import { TranslateModule, TranslateService } from "@ngx-translate/core";
 
 @Component({
-  imports: [TranslateModule],
+  imports: [TranslateModule, RouterLink],
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrl: "./header.component.scss",
 })
 export class HeaderComponent {
-  menuOpen = false;
-
-  constructor(readonly translateService: TranslateService) {}
+  constructor(
+    readonly translateService: TranslateService,
+    readonly router: Router,
+  ) {}
 
   translateText(lang: string) {
     this.translateService.use(lang);
+  }
+
+  goToHome() {
+    this.router.navigate(['/']);
   }
 }
