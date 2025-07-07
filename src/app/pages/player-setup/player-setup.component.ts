fd996e466304b5c042cb63a9886e7b4db81b2c53
Tihomir Denev <tihomir.denev@cleverpine.com>
2025-07-07 12:45:26 +0300

    create player setup

---
+++ src/app/pages/player-setup/player-setup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-setup',
+  imports: [],
+  templateUrl: './player-setup.component.html',
+  styleUrl: './player-setup.component.scss'
+})
+export class PlayerSetupComponent {
+
+}
